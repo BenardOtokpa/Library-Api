15f859b1bb369d8e23cfb42ec6074ec2a824e63d
Benard Otokpa <137802427+BenardOtokpa@users.noreply.github.com>
2024-12-13 15:38:03 +0100

    done with the task

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -17,6 +17,7 @@ exports.addBook = async (req, res) => {
     });
   } catch (err) {
     res.status(500).json({
+      status: "Fail",
       message: "Failed to add book",
       err,
     });
@@ -28,10 +29,14 @@ exports.borrowBook = async (req, res) => {
   // Borrow a new book from the library database
   try {
     const book = await Book.findById(req.params.id);
+
     if (!book) return res.status(404).json({ message: "Book not found" });
     if (book.isBorrowed)
       return res.status(400).json({ message: "Book is already borrowed" });
+
     book.isBorrowed = true;
+    await book.save();
+
     res.status(200).json({
       status: "success",
       message: "Book borrowed successfully",
@@ -41,6 +46,7 @@ exports.borrowBook = async (req, res) => {
     });
   } catch (err) {
     res.status(500).json({
+      status: "Fail",
       message: "Failed to borrow book",
       err,
     });
@@ -49,27 +55,75 @@ exports.borrowBook = async (req, res) => {
 
 //3. Return a book
 exports.returnBook = async (req, res) => {
-  // Add a new book to the library database
-  res.status(200).json({
-    status: "success",
-    message: "Book added successfully",
-  });
+  // Return a book to the library database
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book)
+      return res.status(404).json({
+        message: "Book not found",
+      });
+    if (!book.isBorrowed)
+      return res.status(404).json({
+        status: "Fail",
+        message: "Book is not borrowed",
+      });
+
+    book.isBorrowed = false;
+    await book.save();
+    res.status(200).json({
+      status: "success",
+      message: "Book added successfully",
+      data: {
+        book,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "Fail",
+      message: "Failed to return book",
+      err,
+    });
+  }
 };
 
 // View all available books
 exports.getAllAvailableBooks = async (req, res) => {
-  // Add a new book to the library database
-  res.status(200).json({
-    status: "success",
-    message: "Book added successfully",
-  });
+  //get all available books in the library database
+  try {
+    const books = await Book.find({ isBorrowed: false });
+    res.status(200).json({
+      status: "success",
+      result: books.length,
+      data: {
+        books,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "Fail",
+      message: "Failed to get all available books",
+      err,
+    });
+  }
 };
 
 // View all books
 exports.getAllBooks = async (req, res) => {
   // Add a new book to the library database
-  res.status(200).json({
-    status: "success",
-    message: "Book added successfully",
-  });
+  try {
+    const books = await Book.find();
+    res.status(200).json({
+      status: "success",
+      result: books.length,
+      data: {
+        books,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "Fail",
+      message: "Failed to get all available books",
+      err,
+    });
+  }
 };
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -4,6 +4,10 @@ const booksController = require("../controllers/booksController");
 
 const router = express.Router();
 
+router.get("/", booksController.getAllBooks);
 router.get("/available", booksController.getAllAvailableBooks);
+router.post("/add", booksController.addBook);
+router.patch("/borrow/:id", booksController.borrowBook);
+router.patch("/return/:id", booksController.returnBook);
 
 module.exports = router;
